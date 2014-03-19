cd9d1118e1834714c7ec6a6af772e076ad6cd27d
Daniel Huhn <daniel_huhn@outlook.com>
2014-03-19 11:29:21 +0100

    the nvv parameters are now taken from the http parameters

--- index.js
+++ index.js
@@ -8,10 +8,22 @@ var app = express();
 
 app.get('/Times', function(req, res){
   
-  var from = "Kassel Am Stern";
-  var to = "Wolfhagen Bahnhof";
-
-  var nvvReq = new TimesRequest(from, to, new Date()).createRequest();
+  var startString = req.query.start;
+  if(!startString) startString = new Date().valueOf();
+  
+  var startsInt = parseInt(startString);
+  
+  var from = req.query.from;
+  var to = req.query.to;
+  
+  if(isNaN(startsInt) || !from || !to) {
+    res.send({error: "Invalid parameters!"});
+	return;
+  }
+  
+  var start = new Date(startsInt);
+  
+  var nvvReq = new TimesRequest(from, to, start).createRequest();
   request.post('http://auskunft.nvv.de/nvv/bin/jp/query.exe/dn?L=vs_rmv.vs_nvv&', {form:nvvReq}, function(err, webRes, body){
     var timesParser = new TimesParser(body);
 	var times = timesParser.getTimes();
