45d3487b30294865b2875504c9e6b0a607aed95c
Soeren Wegener <wegener92@gmail.com>
2014-04-13 19:03:26 +0200

    Fixed search for not unique stations.
    Set parameter "REQ0JourneyStopsN: -1" in POST to automatically choose
    first station found by the NVV servers.

--- TripInfo/TripInfoRequest.js
+++ TripInfo/TripInfoRequest.js
@@ -7,22 +7,17 @@ module.exports = function TimesRequest(from, to, date){
   
   self.createRequest = function(){
     return {
-	  REQ0JourneyStopsF: "",
-      REQ0HafasInitialSelection: 0,
+      REQ0JourneyStopsN: -1,
       REQ0JourneyStopsS0A: 255,
       REQ0JourneyStopsS0G: self.from,
-      REQ0JourneyStopsS0ID: "",
       REQ0JourneyStopsZ0A: 255,
       REQ0JourneyStopsZ0G: self.to,
-      REQ0JourneyStopsZ0ID: "",
       REQ0JourneyDateDay: date.getDate(),
       REQ0JourneyDateMonth: date.getMonth()+1,
       REQ0JourneyDateYear: date.getFullYear(),
       REQ0JourneyTimeHour: date.getHours(),
       REQ0JourneyTimeMinutes: date.getMinutes(),
-      REQ0HafasSearchForw: 1,
-      REQ0JourneyProduct_prod_list: "1:1111111111111111",
-      start: "Suchen"
+      start: 'Verbindung suchen'
 	};
   }
-}
+};
