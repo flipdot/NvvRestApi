5d6791951253dcf46e1091d1796348fdc5eb349d
Soeren Wegener <wegener92@gmail.com>
2014-04-13 20:14:24 +0200

    Parse from and to from site.
    To display station correctly, fetch start and destination from the nvv site.

--- TripInfo/TripInfoParser.js
+++ TripInfo/TripInfoParser.js
@@ -1,110 +1,119 @@
 var cheerio = require('cheerio');
 
-module.exports = function(html){
+module.exports = function (html) {
   var self = this;
-  
+
   self.html = html;
-  
-  self.getTimes = function(){
-    var $ = cheerio.load(self.html);
-	
-	var tableBody = $("#HFSResult > table");
-	
-	function getDeparture(row) {
-	  var planed = row
-		.children(".time")
-		.children("div")
-		.children(".planed");
-	  var planedString = planed.text();
-	  var departureRegex = /(\d{2}):(\d{2}) ab/;
-	  var match = departureRegex.exec(planedString);
-	  
-	  if(!match){
-	    return;
-	  }
-	  
-	  var hour = parseInt(match[1]);
-	  var minute = parseInt(match[2]);
-	  
-	  return {
-		hour: hour,
-		minute: minute
-	  }
-	}
-	
-	function getLines(row) {
-	  var linesImageElements = row.children(".products");
-	  
-	  var i = 0;
-	  var current;
-	  var  lineNames = [];
-	  while(current = linesImageElements.children(i)) {
-	    var lineName = current.attr("alt");
-		if(!lineName) break;
-		lineNames.push(lineName);
-	    i++;
-	  }
-	  
-	  return lineNames;
-	}
-	
-	function getDuration(row) {
-	  debugger;
-	  var durationElm = row.children(".duration");
-	  var durationText = durationElm.text();
 
-	  var durationRegex = /(\d{1,2}):(\d{2})/;
-	  var match = durationRegex.exec(durationText);
-	  
-	  if(!match) {
-	    return;
-	  }
-	  
-	  var hours = match[1];
-	  var minutes = match[2];
-	  
-	  return {
-	    hours: hours,
-		minutes: minutes
-	  }
-	}
-	
-	function getSwitchingCount(row) {
-	  var changesElm = row.children(".changes");
-	  var changesText = changesElm.text();
-	  return parseInt(changesText);
-	}
-	
-	function getTripInfo(rowId){
-	  var rowStringId = "#trOverviewC0-"+rowId;
-	  var row = tableBody.children(rowStringId);
+  var $ = cheerio.load(self.html);
 
-	  var departure = getDeparture(row);
-	  var lines = getLines(row);
-	  var duration = getDuration(row);
-	  var switchingCount = getSwitchingCount(row);
-	  
-	  if(!departure || !lines ||
-		 !duration || switchingCount < 0) {
-	    return;
-	  }
-	  
-	  return {
-	    departure: departure,
-		lines: lines,
-		duration: duration,
-		switchingCount: switchingCount
-	  }
-	}
-
-	var trips = [];
-	var i = 0;
-	var current;
-	while(current = getTripInfo(i)){
-	  trips.push(current);
-	  i++;
-	}
-	
-	return trips;
+  self.getFrom = function() {
+    return $('#HFS_from').attr('value');
   };
-}
+
+  self.getTo = function() {
+    return $('#HFS_to').attr('value');
+  };
+
+  self.getTimes = function () {
+
+
+    var tableBody = $("#HFSResult > table");
+
+    function getDeparture(row) {
+      var planed = row
+        .children(".time")
+        .children("div")
+        .children(".planed");
+      var planedString = planed.text();
+      var departureRegex = /(\d{2}):(\d{2}) ab/;
+      var match = departureRegex.exec(planedString);
+
+      if (!match) {
+        return;
+      }
+
+      var hour = parseInt(match[1]);
+      var minute = parseInt(match[2]);
+
+      return {
+        hour: hour,
+        minute: minute
+      }
+    }
+
+    function getLines(row) {
+      var linesImageElements = row.children(".products");
+
+      var i = 0;
+      var current;
+      var lineNames = [];
+      while (current = linesImageElements.children(i)) {
+        var lineName = current.attr("alt");
+        if (!lineName) break;
+        lineNames.push(lineName);
+        i++;
+      }
+
+      return lineNames;
+    }
+
+    function getDuration(row) {
+      debugger;
+      var durationElm = row.children(".duration");
+      var durationText = durationElm.text();
+
+      var durationRegex = /(\d{1,2}):(\d{2})/;
+      var match = durationRegex.exec(durationText);
+
+      if (!match) {
+        return;
+      }
+
+      var hours = match[1];
+      var minutes = match[2];
+
+      return {
+        hours: hours,
+        minutes: minutes
+      }
+    }
+
+    function getSwitchingCount(row) {
+      var changesElm = row.children(".changes");
+      var changesText = changesElm.text();
+      return parseInt(changesText);
+    }
+
+    function getTripInfo(rowId) {
+      var rowStringId = "#trOverviewC0-" + rowId;
+      var row = tableBody.children(rowStringId);
+
+      var departure = getDeparture(row);
+      var lines = getLines(row);
+      var duration = getDuration(row);
+      var switchingCount = getSwitchingCount(row);
+
+      if (!departure || !lines || !duration || switchingCount < 0) {
+        return;
+      }
+
+      return {
+        departure: departure,
+        lines: lines,
+        duration: duration,
+        switchingCount: switchingCount
+      }
+    }
+
+    var trips = [];
+    var i = 0;
+    var current;
+    while (current = getTripInfo(i)) {
+      trips.push(current);
+      i++;
+    }
+
+    return trips;
+  };
+};
--- server.js
+++ server.js
@@ -1,40 +1,42 @@
 var request = require('request');
 var express = require('express');
 
-var TimesRequest = require(__dirname+"/TripInfo/TripInfoRequest.js");
-var TimesParser = require(__dirname+"/TripInfo/TripInfoParser.js");
+var TimesRequest = require(__dirname + "/TripInfo/TripInfoRequest.js");
+var SiteParser = require(__dirname + "/TripInfo/TripInfoParser.js");
 
 var app = express();
 
-app.get('/Trips', function(req, res){
-  
+app.get('/Trips', function (req, res) {
+
   var startString = req.query.start;
-  if(!startString) startString = new Date().valueOf();
-  
+  if (!startString) startString = new Date().valueOf();
+
   var startsInt = parseInt(startString);
-  
+
   var from = req.query.from;
   var to = req.query.to;
-  
-  if(isNaN(startsInt) || !from || !to) {
+
+  if (isNaN(startsInt) || !from || !to) {
     res.send({error: "Invalid parameters!"});
-	return;
+    return;
   }
-  
+
   var start = new Date(startsInt);
-  
+
   var nvvReq = new TimesRequest(from, to, start).createRequest();
-  request.post('http://auskunft.nvv.de/nvv/bin/jp/query.exe/dn?L=vs_rmv.vs_nvv&', {form:nvvReq}, function(err, webRes, body){
-    var timesParser = new TimesParser(body);
-	var times = timesParser.getTimes();
-	
-	res.send({
-	  from: from,
-	  to: to,
-	  times: times
-	});
+  request.post('http://auskunft.nvv.de/nvv/bin/jp/query.exe/dn?L=vs_rmv.vs_nvv&', {form: nvvReq}, function (err, webRes, body) {
+    var parser = new SiteParser(body);
+
+    res.send({
+      from: parser.getFrom(),
+      to: parser.getTo(),
+      times: parser.getTimes()
+    });
   });
 });
 
-app.listen(3000);
+var port = 3001;
+app.listen(port, function () {
+  console.log('Running on localhost:' + port);
+});
 
